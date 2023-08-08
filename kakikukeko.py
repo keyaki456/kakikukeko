1f3e00e19e533664ffa82b5cf446702b58c6028c
keyaki456 <122135530+keyaki456@users.noreply.github.com>
2023-08-08 15:49:04 +0900

    kakikukeko
    kakikukeo

---
+++ kakikukeko.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+
+options = st.multiselect(
+'What are your favorite colors',
+['Green', 'Yellow', 'Red', 'Blue'],
+['Yellow', 'Red'])
+
+st.write('You selected:', options)
